c7cf94fa2e84048a2f0f4bdc558d94a10b85bf69
CodeWizDurham <139919164+CodeWizDurham@users.noreply.github.com>
2024-08-27 09:37:49 -0400

    Update SampleDoorScript.cs

--- CoachLenny_FirstPerson_3D/Assets/Coach Folder/Example Scripts/Interactables/SampleDoorScript.cs
+++ CoachLenny_FirstPerson_3D/Assets/Coach Folder/Example Scripts/Interactables/SampleDoorScript.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
 
+//Before they start they need to have the Animation window and Animator window opened!
+//There are already pre made animations for the door and there is already an animator called "Door"
 //Implementing the interface means we have to call on the custom methods that were made in it!
 public class SampleDoorScript : MonoBehaviour, SampleIInteractable
 {
